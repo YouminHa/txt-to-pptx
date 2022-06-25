e74f9d0900ce8efae95831d0430a4e7ee7b8ccfa
Youmin Ha <youmin78.ha@gmail.com>
2022-06-26 01:01:44 +0900

    add convert-all-txts.sh
    Signed-off-by: Youmin Ha <youmin78.ha@gmail.com>

---
+++ convert-all-txts.sh
@@ -0,0 +1,8 @@
+PROGRAM_DIR=`dirname $0`
+
+txt_files=( `ls *.txt` )
+for f in ${txt_files[@]}
+do
+  out_file="${f%.*}.pptx"
+  python ${PROGRAM_DIR}/txt-to-pptx.py -t ${PROGRAM_DIR}/template.pptx -i ${f} -o ${out_file}
+done
